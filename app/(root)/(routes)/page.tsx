7babaade90244d027302f85443f3bcf1e6f3c3fc
MarianHabor <mariohabor@gmail.com>
2023-08-02 16:37:21 +0100

    new commit

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -14,11 +14,7 @@ const SetupPage = () => {
     }
   }, [isOpen, onOpen]);
 
-  return (
-    <div className="p-4">
-      front page
-    </div>
-  );
+  return null;
 };
 
 export default SetupPage;
