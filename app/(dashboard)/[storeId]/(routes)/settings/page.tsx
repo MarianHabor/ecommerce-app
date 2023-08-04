46caa5a4cd06f236b5a7b72e87e9c96cef31e598
MarianHabor <mariohabor@gmail.com>
2023-08-04 18:23:14 +0100

    new functionality

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -2,6 +2,7 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
+import { SettingsForm } from "./components/settings-form";
 
 interface SettingPageProps {
     params: {
@@ -30,8 +31,10 @@ const SettingsPage: React.FC<SettingPageProps> = async ({
     }
 
     return (
-        <div>
-            Settings
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <SettingsForm initialData={store} />
+            </div>
         </div>
     );
 }
